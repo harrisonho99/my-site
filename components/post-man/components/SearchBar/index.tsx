8642df0dccf4c158d48f666bdb66504354068e66
Hồ Sỹ Nhật Hoàng <hoanghsn@blackwind.vn>
2021-06-26 04:41:19 +0700

    build

--- components/post-man/components/SearchBar/index.tsx
+++ components/post-man/components/SearchBar/index.tsx
@@ -2,24 +2,28 @@ import { PureComponent } from 'react';
 import MethodSelector from '../common/MethodSelector';
 import TextField from '../InputBase/TextField';
 import HighlightBtn from '../ButtonBase/HighlightBtn';
-import { useMediaQuery } from '@material-ui/core';
+import { useMediaQuery, Container, Paper } from '@material-ui/core';
+
 class SearchBar extends PureComponent {
   render = () => {
     const { matches } = this.props as any;
     return (
-      <div className='mt-20 pl-4 pr-4 flex flex-wrap'>
-        <MethodSelector />
-        <div className='flex-1 flex pl-2' style={{ minWidth: 200 }}>
-          <TextField />
-        </div>
-        <div
-          className={`flex justify-center items-center h-full ${
-            matches ? 'mt-3 w-full' : 'ml-2'
-          } `}
-        >
-          <HighlightBtn title='SEND' />
-        </div>
-      </div>
+      <Container className='mt-20' maxWidth='md'>
+        <Paper>
+          <div className='flex flex-nowrap'>
+            <MethodSelector />
+            <div
+              className='flex-1 flex pl-2 justify-center items-center'
+              style={{ maxWidth: 700 }}
+            >
+              <TextField />
+            </div>
+            <div className={`flex justify-center items-center h-full ml-2`}>
+              <HighlightBtn title='SEND' />
+            </div>
+          </div>
+        </Paper>
+      </Container>
     );
   };
 }
