6c671761631834ac729e6f4a2fd84a5ceab3366f
Hồ Sỹ Nhật Hoàng <hoanghsn@blackwind.vn>
2021-06-26 04:05:59 +0700

    build

--- components/post-man/components/ButtonBase/HighlightBtn.tsx
+++ components/post-man/components/ButtonBase/HighlightBtn.tsx
@@ -6,7 +6,7 @@ interface Props {
 export default function HighlightBtn({ title = '', type = 'submit' }: Props) {
   return (
     <button
-      className='bg-indigo-600 dark:bg-blue-700 h-10 rounded-md w-20 font-semibold text-white'
+      className='transform duration-300 transition-colors bg-indigo-600 hover:bg-indigo-800 dark:bg-blue-700 h-10 rounded-md w-20 font-semibold text-white'
       type={type}
     >
       {title}
